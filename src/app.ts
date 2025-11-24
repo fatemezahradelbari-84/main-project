1e34040054c53c585a66be6f7ad2312ae826c32f
fatemezahra <fatemezahra.delbar8@gmail.com>
2025-11-24 12:40:17 +0330

    configure database connecting

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import authorRoutes from './routes/authors';
+import connectDB from './config/database';
+
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+connectDB();
 
 app.use(express.json());
 
