2be56a161a6ae1eaaf70052390ce5d101d32b14f
fatemezahra <fatemezahra.delbar8@gmail.com>
2025-11-29 19:59:13 +0330

    add validation for age

--- src/models/author.ts
+++ src/models/author.ts
@@ -1,4 +1,6 @@
 import { Schema, model, Document } from 'mongoose';
+import * as v from 'valibot';
+
 
 export interface IAuthor extends Document {
   name: string;
@@ -11,7 +13,17 @@ const authorSchema = new Schema({
   name: { type: String, required: true },
   family: { type: String, required: true },
   gender: { type: String, enum: ['male', 'female'], required: true },
-  age: { type: Number, required: true }
+  age: { 
+    type: Number, 
+    required: true,
+    min: [1, 'سن نویسنده نمی‌تواند کمتر از 1 باشد'],
+    max: [120, 'سن نویسنده نمی‌تواند بیشتر از 120 باشد'],
+    validate: {
+      validator: Number.isInteger,  // فقط اعداد صحیح
+      message: 'سن باید یک عدد صحیح باشد'
+    }
+  }
 }, { timestamps: true });
+
 
 export default model<IAuthor>('Author', authorSchema);
