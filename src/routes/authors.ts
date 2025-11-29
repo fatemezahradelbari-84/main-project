1ef074f9c74f960030dcf1124d823fc55f7d0f8e
fatemezahra <fatemezahra.delbar8@gmail.com>
2025-11-29 20:49:07 +0330

    edit name

--- src/routes/authors.ts
+++ src/routes/authors.ts
@@ -9,24 +9,16 @@ import {
 
 const router = express.Router();
 
-// ایجاد نویسنده (POST)
-// body: { name, family, gender, age, db? }
+
 router.post('/', createAuthor);
 
-// دریافت همه نویسنده‌ها (GET)
-// query: ?db=mongo یا ?db=mysql
+
 router.get('/', getAuthors);
 
-// دریافت نویسنده با ID (GET)
-// query: ?db=mongo یا ?db=mysql
 router.get('/:id', getAuthorById);
 
-// بروزرسانی کامل نویسنده (PUT)
-// body: { name, family, gender, age, db? }
 router.put('/:id', updateAuthor);
 
-// حذف نویسنده (DELETE)
-// query: ?db=mongo یا ?db=mysql
 router.delete('/:id', deleteAuthor);
 
 export default router;
