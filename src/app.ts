e4d6f0cd387b3ad5418521494cc3f15c9c8b04ff
fatemezahra <fatemezahra.delbar8@gmail.com>
2025-11-29 15:21:03 +0330

    update database in app

--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,30 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import authorRoutes from './routes/authors';
-import connectDB from './config/database';
 import bookRoutes from './routes/books';
-
+import { connectDatabase } from './config/database';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-connectDB();
+(async () => {
+    await connectDatabase(); 
+    app.use(express.json());
 
-app.use(express.json());
+    // Routes
+    app.use('/api/authors', authorRoutes);
+    app.use('/api/books', bookRoutes);
 
-// Routes
-app.use('/api/authors', authorRoutes);
-app.use('/api/books', bookRoutes);
-
-app.get('/', (req, res) => {
-    res.json({ 
-        message: "Library API is running!",
-        status: "OK"
+    app.get('/', (req, res) => {
+        res.json({ 
+            message: "Library API is running!",
+            status: "OK"
+        });
     });
-});
 
-app.listen(PORT, () => {
-    console.log(`🚀 Server running on port ${PORT}`);
-});
+    app.listen(PORT, () => {
+        console.log(`🚀 Server running on port ${PORT}`);
+    });
+})();
--- src/config/database.ts
+++ src/config/database.ts
@@ -1,34 +1,15 @@
-import mongoose from 'mongoose';
+import { versionConfig } from "./version";
+import { connectMongo } from "./mongo";
+import { connectMySql } from "./mySql";
 
-const connectDB = async (): Promise<void> => {
-    try {
-        const mongoURI = process.env.MONGODB_URI || process.env.MONGO_URI || 'mongodb://localhost:27017/library-project';
-        
-        const conn = await mongoose.connect(mongoURI);
-        
-        console.log(`MongoDB Connected Successfully: ${conn.connection.host}`);
-        console.log(`Database Name: ${conn.connection.name}`);
-        
-    } catch (error) {
-        console.error('Database connection failed:');
-        
-        if (error instanceof Error) {
-            console.error(`Error Message: ${error.message}`);
-        } else {
-            console.error('Unknown error:', error);
-        }
-        
-        // Exit process with failure
-        process.exit(1);
-    }
+export const connectDatabase = async () => {
+  const engine = versionConfig.getDbEngine();
+
+  if (engine === "mongo") {
+    await connectMongo();
+  } else if (engine === "mysql") {
+    await connectMySql();
+  } else {
+    throw new Error("Unknown database engine");
+  }
 };
-
-mongoose.connection.on('disconnected', () => {
-    console.log('MongoDB disconnected');
-});
-
-mongoose.connection.on('error', (err) => {
-    console.error('MongoDB connection error:', err);
-});
-
-export default connectDB;
